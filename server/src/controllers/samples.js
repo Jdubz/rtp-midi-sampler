7cb0ae32ad372423dd017b6b900e4726791f7a47
sampler <sampler@joshwentworth.com>
2022-02-07 07:53:47 +0000

    get Samples

--- server/src/controllers/samples.js
+++ server/src/controllers/samples.js
@@ -1,24 +1,12 @@
-var fs = require('fs');
-
-const walkDir = async (dir, results) => {
-  const files = await fs.promises.readdir(dir)
-  files.forEach((file) => {
-    const stat = await fs.promises.stat(file);
-    if (stat && stat.isDirectory()) {
-      const directory = {};
-      directory.name = file;
-      directory.files = await walkDir(file, results);
-      results.push(directory);
-    } else {
-      results.push(file);
-    }
-    return results;
-  });
-}
 
 class SamplesController {
-  readSamples = async (dir) => {
-    return await walkDir(dir);
+  constructor(service) {
+    this.service = service;
+  }
+
+  getSamples = async (req, res) => {
+    const samples = await this.service.readSamplesFolder();
+    res.send(samples);
   }
 };
 
--- server/src/services/samples.js
+++ server/src/services/samples.js
@@ -1,5 +1,25 @@
-class Samples {
+var fs = require('fs');
+
+const walkDir = async (dir, results) => {
+  const files = await fs.promises.readdir(dir)
+  files.forEach((file) => {
+    const stat = await fs.promises.stat(file);
+    if (stat && stat.isDirectory()) {
+      const directory = {};
+      directory.name = file;
+      directory.files = await walkDir(file, results);
+      results.push(directory);
+    } else {
+      results.push(file);
+    }
+    return results;
+  });
+}
+
+class SamplesService {
   readSamplesFolder() {
-    
+    return await walkDir(dir);
   }
 }
+
+module.exports = SamplesService;
